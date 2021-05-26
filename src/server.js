859d69c10cb2d21ddca4237898c62a8b7daf01dd
sw100 <dreaming100sw@gmail.com>
2021-05-26 22:20:06 +0900

    라우터, 컨트롤러 정리

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import morgan from 'morgan';
+import globalRouter from './routers/globalRouter';
+import userRouter from './routers/userRouter';
+import videoRouter from './routers/videoRouter';
 
 const PORT = 4000;
 
@@ -7,25 +10,7 @@ const app = express();
 const logger = morgan('dev');
 app.use(logger);
 
-const globlaRouter = express.Router();
-
-const handleHome = (req, res) => res.send('home');
-
-globlaRouter.get('/', handleHome);
-
-const userRouter = express.Router();
-
-const handleEditUser = (req, res) => res.send('Edit User');
-
-userRouter.get('/edit', handleEditUser);
-
-const videoRouter = express.Router();
-
-const handleWatchVideo = (req, res) => res.send('Watch Video');
-
-videoRouter.get('/watch', handleWatchVideo);
-
-app.use('/', globlaRouter);
+app.use('/', globalRouter);
 app.use('/videos', videoRouter);
 app.use('/users', userRouter);
 
